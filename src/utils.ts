ebd2955a1de8088a43135b645423d61d97f8c582
rob <robmathew21@gmail.com>
2025-03-24 17:02:00 -0500

    capitalized first letter of tag and added #

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// Format date to str
 function formatDate(date: Date): string {
     const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'long',
     day: 'numeric'};
@@ -5,4 +6,12 @@ function formatDate(date: Date): string {
     return new Date(date).toLocaleDateString(undefined, options);
   }
 
-  export { formatDate };
+  //Capitalize the first letter
+  function capitalize(str: string): string {
+    if (typeof str !== 'string' || str.length === 0) {
+      return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  export { formatDate, capitalize };
