387eca57c83ba5f44ab99cb997916b632c22a787
rob <robmathew21@gmail.com>
2025-03-25 22:45:18 -0500

    added try...catch to search.json, added null,2 to JSON.stringify for better formatting, to address 500 deployment error to vercel

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -3,10 +3,11 @@ import { getCollection } from 'astro:content';
 import type { CollectionEntry } from 'astro:content'
 
 export const GET: APIRoute = async ({ url }): Promise<Response> => {
-    const query: string | null = url.searchParams.get('query');
-    
-    // Handle if query is not present
-    if (query === null) {
+    try {
+        const query: string | null = url.searchParams.get('query');
+
+        // Handle if query is not present
+        if (!query) {
         return new Response(JSON.stringify({
             error: 'Query param if missing'
         }), {
@@ -16,26 +17,25 @@ export const GET: APIRoute = async ({ url }): Promise<Response> => {
             }
         });
     }
-
     const allBlogArticles: CollectionEntry<'blog'>[] = await getCollection('blog');
 
-    // Filter articles based on query
-    const searchResults = allBlogArticles.filter(article => {
-    const titleMatch: boolean = article.data.title
-    .toLowerCase()
-    .includes(query!.toLowerCase());
-
-
-    const bodyMatch: boolean = article.body
-    .toLowerCase()
-    .includes(query!.toLowerCase());
-
-    const slugMatch: boolean = article.slug
-    .toLowerCase()
-    .includes(query!.toLowerCase());
-
-    return titleMatch || bodyMatch || slugMatch
-});
+     // Filter articles based on query
+     const searchResults = allBlogArticles.filter(article => {
+        const titleMatch = article.data.title
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    
+    
+        const bodyMatch = article.body
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    
+        const slugMatch = article.slug
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    
+        return titleMatch || bodyMatch || slugMatch;
+    });
 
     return new Response(JSON.stringify(searchResults, null, 2), {
         status: 200, 
@@ -43,4 +43,13 @@ export const GET: APIRoute = async ({ url }): Promise<Response> => {
             'Content-Type': 'application/json'
         }
     });
+
+    } catch (error) {
+        console.error('API Error:', error);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
 };
